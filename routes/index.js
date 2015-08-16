495ca7696a0c7bfd3f7e6336432ecfa630004b9f
btroo <me@brandontruong.com>
2015-08-16 16:05:03 -0500

    added tmdb stuff

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var request = require('request');
+var async = require('async');
+
 var router = express.Router();
 
-/* GET home page. */
+var tmdbKey = process.env.TMDB_KEY;
+var tmdbUrl = 'http://api.themoviedb.org/3/';
+
+function makeUrl(ext) {
+	return tmdbUrl + ext + '?api_key=' + tmdbKey;
+}
+
+/* GET home page. 
+ * Display currently showing movies as well as the ages and average.
+ */
 router.get('/', function(req, res, next) {
+  
   res.render('index', { title: 'Express' });
 });
 
