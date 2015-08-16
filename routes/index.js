78100a81dc9dec3b91068d9c37822688913eeb24
btroo <me@brandontruong.com>
2015-08-16 16:50:59 -0500

    laying out functions

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,26 @@ function makeUrl(ext) {
 	return tmdbUrl + ext + '?api_key=' + tmdbKey;
 }
 
+router.get('/', function(req, res, next) {
+	res.redirect('/1');
+});
+
 /* GET home page. 
  * Display currently showing movies as well as the ages and average.
  */
-router.get('/', function(req, res, next) {
+router.get('/:page', function(req, res, next) {
+  var currentPage = req.params.page;
+  var movies = [];
+  request(makeUrl('movie/now_playing'), function(error, response, body) {
+
+  });
   
   res.render('index', { title: 'Express' });
 });
+
+router.get('/movie/:id', function(req, res, next) {
+
+	res.render('movie', {});
+})
 
 module.exports = router;
