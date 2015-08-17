9d57ee7c7334604860c231d5428bbf28d9046d06
btroo <me@brandontruong.com>
2015-08-16 20:09:59 -0500

    added some styles with materialize

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
 	$('.movie-btn').click(function(e) {
 		var id = $(this).data('movid');
-		
+		var that = this;
+
+		$(that)
+		 .parent()
+		 .parent()
+		 .children('.bottom')
+		 .children('.age')
+		 .html('<div class="preloader-wrapper big active"><div class="spinner-layer spinner-blue-only">' +
+			     '<div class="circle-clipper left">' +
+			      	'<div class="circle"></div>' +
+			     '</div><div class="gap-patch">' +
+			        '<div class="circle"></div>' +
+			     '</div><div class="circle-clipper right">' +
+			        '<div class="circle"></div>' +
+			     '</div></div></div>');
+
 		$.ajax({
 			url: '/movie/' + id,
 			success: function(data, status, obj) {
-				alert(JSON.stringify(data));
+				var age = (data.avg === 0) ? "Not available" : data.avg.toString().substr(0, 4);
+				$(that)
+				 .parent()
+				 .parent()
+				 .children('.bottom')
+				 .children('.age')
+				 .html('<h2>' + age + '</h2>');
 			}			
 		})
 
